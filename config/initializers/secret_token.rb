d18e1c9689df4807a474269127753f195a85e7e7
Paul ADENOT <3voker@gmail.com>
2010-09-23 15:00:22 +0200

    Application structure + hello world

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railstraining::Application.config.secret_token = 'e0bd8782e06d4fb11dfe6c93f66bc288f3280bcfa6175a7756d22f134d514440a81c51bea89b9efd9ddcf6bf0be75ee86375502a327387cb512d3257b57aedaf'
